029219f1b653cbb33211a1c5c4293f7efd762d06
Mike3285 <m.romanin94@gmail.com>
2021-07-07 18:56:53 +0200

    form e pagina eliminazione professore
    (cherry picked from commit a92c0ffc4d57ae721fa70ffe251d14d31e3b512d)
    
    # Conflicts:
    #	db.sqlite3
    #	registro_scolastico/forms.py
    #	registro_scolastico/views.py
    #	templates/components/footer.html
    #	templates/components/navbar.html
    #	templates/form_test.html

--- registro_scolastico/views.py
+++ registro_scolastico/views.py
@@ -2,61 +2,36 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from registro_scolastico.models import *
 from django.db.models import ObjectDoesNotExist
-from registro_scolastico.forms import *
+from registro_scolastico.forms import MyFirstForm
 import random
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
-from django.core.paginator import Paginator
 
-# homepage
+
 def index(request):
     return render(request, 'home.html')
-
-
-# Sezione studenti
-def infostudenti(request):
-    return render(request, "pagina_studenti.html")
 
 
 def studenti(request):
     studs = Studente.objects.all()
     context = {'studs': studs}
-    paginator = Paginator(studs, 25)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    context.update({'paginazione': page_obj})
     return render(request, 'studenti.html', context)
-
-
-def elimina_studente(request, id_studente):
-    studente = Studente.objects.get(id=id_studente)
-    studente.delete()
-    return render(request, "system_studenti.html")
-
-
-def ricerca_studente(request):
-    context = {"ricerca": RicercaStudente}
-    if request.POST:
-        dati = dict(request.POST)
-        studente = Studente.objects.get(cognome__iexact=dati["cognome"])
-        context["studente"] = studente
-    return render(request, "ricerca_studente.html", context)
-
-
-def dettaglio_studente(request, id_studente):
-    context = {'stud': Studente.objects.get(id=id_studente)}
-    return render(request, 'dettaglio_studente.html', context)
-
-
-# Sezione professori
-def infodocenti(request):
-    return render(request, "pagina_docenti.html")
 
 
 def professori(request):
     profs = Professore.objects.all()
     context = {'profs': profs}
     return render(request, 'professori.html', context)
+
+
+def dettaglio_studente(request, id_studente: str):
+    stud = Studente.objects.none()
+    if id_studente.isdigit():
+        stud = Studente.objects.get(id=id_studente)
+    elif id_studente.isalpha():
+        stud = Studente.objects.filter(nome__icontains=id_studente)
+    context = {'stud': stud}
+    return render(request, 'dettaglio_stud.html', context)
 
 
 def dettaglio_materia(request, nome_materia: str):
@@ -93,20 +68,9 @@ def form_test_view(request):
         creasion = Professore.objects.create(nome=dati["nome"][0], cognome=dati["cognome"][0], email=dati["email"][0])
         creasion.save()
         context["new_prof"] = creasion
+
+
     return render(request, 'form_test.html', context)
-
-
-def iscrizionestudente(request):
-    context = {"iscr_stud": StudentForm}
-    if request.POST:
-        dati = dict(request.POST)
-        iscrizione = Studente.objects.create(nome=dati["nome"][0], cognome=dati["cognome"][0], email=dati["email"][0])
-        for materia_id in dati["competenze"]:
-            k = Materia.objects.get(pk=int(materia_id))
-            iscrizione.competenze.add(k)
-        iscrizione.save()
-        context["new_stud"] = iscrizione
-    return render(request, "iscrizione_studente.html", context)
 
 
 def elimina_prof(request, ilprof):
