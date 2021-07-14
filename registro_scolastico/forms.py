015c678a9f5af1b8df179281c432c04f3d083eaf
Mike3285 <m.romanin94@gmail.com>
2021-07-14 14:42:19 +0200

    Ripristinate pagine studente di stiefin

--- registro_scolastico/forms.py
+++ registro_scolastico/forms.py
@@ -1,7 +1,18 @@
 from django import forms
+from registro_scolastico.models import *
 
 
 class MyFirstForm(forms.Form):
     nome = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
     cognome = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control"}))
+
+
+class StudentForm(forms.Form):
+    nome = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
+    cognome = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control"}))
+    competenze = forms.ModelMultipleChoiceField(queryset=Materia.objects.all(), widget=forms.SelectMultiple(attrs={"class": "form-control"}))
+
+class RicercaStudente(forms.Form):
+    cognome = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
